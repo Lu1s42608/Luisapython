ac0352e0836aecf9f8001bcc9fd87e571b2334cb
Lu1s42608 <luisafbm263@gmail.com>
2024-08-12 08:36:14 -0500

    Add files via upload

---
+++ luisa python 14.py
@@ -0,0 +1,7 @@
+numero1 = float(input("ingresar numero"))
+numero2= float(input("ingresar numero"))
+if numero2 == 0:
+  print("error")
+else :
+ division = numero1 / numero2
+print("la division de", numero1, "la division de", numero2 , "es:", division)
